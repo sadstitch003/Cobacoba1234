679282f23b79eebbb354ace6c5a3665cde9f2fa0
Christian Sad <christian.kartika1@gmail.com>
2021-12-03 15:14:22 +0700

    Menghapus hello world dan menulis program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                var input = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Masukkan input : ");
+
+                var jumlahPembagi = 0;
+                for (int i = 1; i <= input; i++)
+                    if (input % i == 0) jumlahPembagi++;
+
+                if (jumlahPembagi == 2)
+                    Console.WriteLine("PRIMA\n");
+                else
+                    Console.WriteLine("BUKAN PRIMA\n");
+
+                Console.WriteLine("Tekan keyboard untuk melanjutkan");
+                Console.ReadKey();
+            }
+        }
+    }
+}
